1351ccf680fea9f45be1a4d883cba2a036dff133
김현준 <agemor@naver.com>
2014-02-03 00:58:50 +0900

    android version updated.

---
+++ sealion client (android)/src/sealion/client/Launcher.java
@@ -0,0 +1,28 @@
+package sealion.client;
+
+import sealion.client.android.ActionResolver;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+
+import android.os.Bundle;
+
+public class Launcher extends AndroidApplication {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		ActionResolver.initialize(this);
+
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		config.useGL20 = true;
+		config.useWakelock = true;
+
+		Main main = new Main();
+		Main.self = main;
+
+		initialize(main, config);
+	}
+
+}
